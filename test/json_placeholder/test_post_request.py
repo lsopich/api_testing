bc39c85d3a973a7955a4d7075b342e391b945227
lsopich <89616762+lsopich@users.noreply.github.com>
2022-08-28 20:10:05 -0400

    Add files via upload

---
+++ test/json_placeholder/test_post_request.py
@@ -0,0 +1,18 @@
+import requests
+import pytest
+from resources.json_placeholder_enum import PlaceholderEnum
+
+
+@pytest.mark.jsonplaceholdertestpost
+def test_post_request():
+    my_obj ={
+        "userId": 1,
+        "title": "my title",
+        "body": "body example"
+    }
+    response = requests.post(PlaceholderEnum.JsonPost.value, data=my_obj)
+    assert response.status_code == 201
+
+
+
+
